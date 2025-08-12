9e9cc055eb9b5f2259f5caa36d29f12bdd84f16f
Ankit-Keshri-QA <ankitkeshri198@gmail.com>
2025-08-12 17:05:02 +0530

    Parameter added in pom.xml

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -2,14 +2,32 @@ package mavenforjenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class UITest {
 
+    @Parameters("Browser")
     @Test
-    public void startBrowser() {
-        WebDriver driver = new ChromeDriver();
+    public void startBrowser(String browserName) {
+        System.out.println("Parameter name is : " + browserName);
+        WebDriver driver = null;
+        switch (browserName) {
+            case "CHROME":
+                driver = new ChromeDriver();
+                break;
+            case "EDGE":
+                driver = new EdgeDriver();
+                break;
+            case "FIREFOX":
+                driver = new FirefoxDriver();
+                break;
+            default:
+                throw new IllegalArgumentException("INVALID BROWSER NAME: " + browserName);
+        }
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         Assert.assertEquals(driver.getTitle(), "OrangeHRM", "Title Mismatch ..");
